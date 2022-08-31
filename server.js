8d69e47c7e4672df4b6c5bded6b731283d127684
Julian Martinez <julian.martinez.cs@gmail.com>
2022-08-31 01:54:59 +0100

    Trying to get the server to give me a 200 status

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = require('./server/app');
-const port = 3001;
+const db = require('./server/db')
+// const port = 3001;
+const PORT = process.env.PORT || 3001;
 
+// app.listen(()=>{
+//     console.log(`App listening on port ${port}`)
+// })
 
+const init = async () => {
+  try {
+    await db.sync();
 
+    app.listen(PORT, () => {
+      console.log(`Server listening at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error starting server:', error)
+  }
+};
 
-app.listen(()=>{
-    console.log(`App listening on port ${port}`)
-})
+init();
+
+//Cuerrently getting a 500 server internal error, trying to sync the servers
--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (res, req) => {
+    res.sendStatus(200);
+})
 
 
 module.exports = app
