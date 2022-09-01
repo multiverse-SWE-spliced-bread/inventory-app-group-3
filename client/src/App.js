642eaa0ad7f8a32ca108677420048a1fc3b6cf39
Julian Martinez <julian.martinez.cs@gmail.com>
2022-09-01 16:59:51 +0100

    Tier 1 completed

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ export const App = () => {
   }
 
   const clicked = () => {
-        
+    setResourceType('All your Items')
     setViewItems(true) 
 }
 
@@ -34,16 +34,16 @@ export const App = () => {
         <div>
           <h1>Inventory App</h1>
           <h3>Please click on the buttons to retrieve information</h3>
-          <button className='btn' onClick={()=> setResourceType('These are all your Itmes')}>View All Items</button>
+          <button className='btn' onClick={()=> clicked()}>View All Items</button>
           <button className='btn' onClick={()=> setResourceType('Here there is a single Item')}>View a Single Item</button>
           <button className='btn' onClick={()=> setResourceType('Please add Item')}>Add an Item</button>
           <button className='btn' style={{background: 'red'}} onClick={()=> setResourceType('Are you sure you want to delete an Item?')}>Remove an Item</button>
           <button className='btn' onClick={()=> setResourceType('Edit or update an Item')}>Edit or Update</button>
         </div>
         <h1>{resoursceType}</h1>
-        <ItemsList items={items} setItems={setItems} fetchItems={fetchItems}/>
-        {viewItems ? {resoursceType} : <ItemsList items={items} setItems={setItems} fetchItems={fetchItems}/> }
-        <button className='btn' onClick={clicked}>Create Page</button>
+        {
+        (viewItems) && <ItemsList items={items} setItems={setItems} fetchItems={fetchItems}/>
+        }
       </main>
     
   );
