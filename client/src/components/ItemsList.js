681bf7f910bc19d6c3a61ef259a05370c029614f
Julian Martinez <julian.martinez.cs@gmail.com>
2022-09-01 16:15:21 +0100

    Data showing in Fron-End

--- client/src/components/ItemsList.js
+++ client/src/components/ItemsList.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 export const ItemsList = ({items}) => {
-    return<>
-    {
-        items.map((item, idx) => {
-            return <Item item={item} key={idx}/>
-        })
-    }
-    
-    </>
+    return(<>
+        {
+            items.map((item, idx) => {
+                return <>
+                    <h3>{item.title}</h3>
+                </>
+            })
+        }
+    </>)
 }
