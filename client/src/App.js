d1c4e2b308c89053a938b6e7e0c039c57f5a4af2
tlndou <thendo@hotmail.co.uk>
2022-09-01 15:00:49 +0100

    something

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { ItemsList } from './ItemsList'
 import apiURL from './api';
 
 
 export const App = () => {
   
-  const [resourscType, setResourceType] = useState('Items')
+  const [resourscType, setResourceType] = useState(0)
   const [items, setItems] = useState ([]);
   
   async function fetchItems(){
@@ -26,11 +27,11 @@ export const App = () => {
       <main className='container'>
         <h1>Inventory App</h1>
         <h3>Please click on the buttons to retrieve information</h3>
-        <button className='btn' onClick={()=> setResourceType('items')}>View All Items</button>
-        <button className='btn' onClick={()=> setResourceType('singleItem')}>View a Single Item</button>
+        <button className='btn' onClick={()=> setResourceType(1)}>View All Items</button>
+        {/* <button className='btn' onClick={()=> setResourceType('singleItem')}>View a Single Item</button>
         <button className='btn' onClick={()=> setResourceType('addItem')}>Add an Item</button>
         <button className='btn' style={{background: 'red'}} onClick={()=> setResourceType('removeItem')}>Remove an Item</button>
-        <button className='btn' onClick={()=> setResourceType('editUpdate')}>Edit or Update</button>
+        <button className='btn' onClick={()=> setResourceType('editUpdate')}>Edit or Update</button> */}
       </main>
     
   );
---
+++ client/src/ItemsList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export const ItemsList = ({items}) => {
+	// // the data that will run when a title is clicked
+	// const clickHandler = (item) => {
+	// 	// changes the current page. afaik, the main page is PagesList, so I setCurrentPage(0) in App.js which represents PagesList.
+	// 	setCurrentPage(1)
+	// 	// this will make the page data visible
+	// 	setViewPage(page)
+
+	// }
+
+    // const returnHandler = (homepage) => {
+	// 	// changes the homepage.
+	// 	setCurrentPage(0)
+	// 	// this will make the page data visible
+	// 	setViewPage({})
+
+	// }
+
+
+	return <>
+		{
+			items.map((item, i) => {
+				return <h3 key={i}
+				>{items.title}</h3>
+			})
+		}
+	</>
+} 
